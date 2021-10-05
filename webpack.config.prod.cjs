ba4c07c70a8a0a176c51061c1a15f08413e03cd8
J&M design <86296275+ar15ak23@users.noreply.github.com>
2021-10-05 12:23:27 +0530

    Add files via upload

---
+++ webpack.config.prod.cjs
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    'mindar-image': './src/image-target/index.js',
+    'mindar-image-aframe': './src/image-target/aframe.js',
+    'mindar-face': './src/face-target/index.js',
+    'mindar-face-aframe': './src/face-target/aframe.js',
+  },
+  mode: 'production',
+  output: {
+    filename: '[name].prod.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: ''
+  },
+  module: {
+    rules: [
+      {
+        test: /\.worker\.js$/,
+        use: {
+          loader: 'worker-loader',
+          options: {
+            inline: true,
+            name: '[name].prod.js'
+          },
+        },
+      },
+      {
+	test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+	  'sass-loader'
+        ]
+      },
+      {
+	test: /\.html$/,
+        use: 'html-loader',
+      }
+    ],
+  }
+};
